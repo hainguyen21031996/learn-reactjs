26183edb370ac195f92c2a5c218388f3ec275f0f
Hai Nguyen <hainguyen21031996@gmail.com>
2021-06-03 19:41:47 +0700

    Update Now

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = 'Hải';
+  const age = 25;
+  const isMale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+
+  const Listcolor = ['red', 'blue', 'yellow'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hải Nguyễn (HaiKunBoiz) - Mlem Mlem
+          Hải Nguyễn (HaiKunBoiz)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name} - {age} - {isMale ? 'Male' : 'Female'} - Xin chào</p>
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </>
+        )}
+
+        <p>
+          {student.name}
+        </p>
+
+        <ul>
+          {Listcolor.map(color => (
+            <li style={{ color }}>
+              {color}
+            </li>
+          ))}
+        </ul>
+
       </header>
     </div>
   );
