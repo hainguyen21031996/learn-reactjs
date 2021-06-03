26c1309d1a291565e86456bb925b6361abf29752
Hai Nguyen <hainguyen21031996@gmail.com>
2021-06-03 16:57:16 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hải Nguyễn
+          Hải Nguyễn (HaiKunBoiz)
         </p>
         <a
           className="App-link"
